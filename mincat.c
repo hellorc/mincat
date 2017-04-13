af34f95758ca536038167d4d23f984e596658912
hellorc32 <hellorc32@gmail.com>
2017-04-13 15:43:54 +0530

    added argument functionality

--- mincat.c
+++ mincat.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
 
 void file_copy(FILE *, FILE *);
 
@@ -9,9 +13,24 @@ int main(int argc, char const *argv[])
 
 	if(argc == 1) file_copy(stdin,stdout);
 
-	else
-		while(--argc > 0)
-			if( (fp = fopen(*++argv,"r")) ==NULL ) {
+	else{
+
+		while(--argc > 0){
+			if( (*++argv)[0]=='-' ){
+				int s=strlen(*argv);
+				while(s-- > 1) 
+					switch( (*argv)[s] ){
+
+						
+						default : printf("mincat: invalid option -- '%c'\n",(*argv)[s]);exit(0);
+					}
+			}
+			else 
+				break;
+        }
+       
+		while(argc-- > 0)
+			if( (fp = fopen(*argv++,"r")) ==NULL ) {
 				printf("can't open %s\n",*argv );
 				return 1;
 			}
@@ -20,6 +39,7 @@ int main(int argc, char const *argv[])
 				file_copy(fp,stdout);
 				fclose(fp);
 			}
+	}
 
 	return 0;
 }
@@ -28,5 +48,11 @@ void file_copy(FILE * ifp, FILE *ofp)
 {
 	int c;
 
-	while( (c = getc(ifp)) !=EOF ) putc(c,ofp);
+	while( (c = getc(ifp)) !=EOF ) {
+		
+		
+
+		putc(c,ofp);
+	}
+
 }
